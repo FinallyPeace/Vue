b3830301640b01d68be11bdc623272232c63ef8e
FinallyPeace <ll7856l@gmail.com>
2021-05-18 00:56:14 +0800

    add day6

---
+++ 2. 使用者輸入 列表呈現/vue.js
@@ -0,0 +1,34 @@
+let data = {
+    input: {
+        type: '主餐',
+        content: ''
+    },
+    menu: [
+        { type: '沙拉', content: '煙燻鮭魚', link: '/' },
+        { type: '沙拉', content: '炭烤雞肉', link: 'javascript:;' },
+        { type: '主餐', content: '德國豬腳', link: 'javascript:;' },
+        { type: '主餐', content: '法式羊排', link: 'javascript:;' },
+        { type: '甜點', content: '焦糖布蕾', link: 'javascript:;' },
+        { type: '甜點', content: '抹茶冰淇淋', link: 'javascript:;' },
+        { type: '飲料', content: '長島冰茶', link: 'javascript:;' },
+        { type: '飲料', content: 'Mojito', link: 'javascript:;' },
+        
+    ]
+}
+
+let vm = new Vue({
+    el: '#app',
+    data: data,
+    methods: {
+        inputHandler() {
+            if (this.input.content) {
+                this.menu.push({
+                    type: this.input.type,
+                    content: this.input.content,
+                    link: 'javascript:;'
+                })
+                this.input.content = ''
+            }
+        }
+    }
+})
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Vue
+# 常用框架與套件
+
+```html
+<!-- css -->
+<link rel="stylesheet" href="../base.css" />
+
+<!-- Vue -->
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.js"
+    integrity="sha512-YXLGLsQBiwHPHLCAA9npZWhADUsHECjkZ71D1uzT2Hpop82/eLnmFb6b0jo8pK4T0Au0g2FETrRJNblF/46ZzQ=="
+    crossorigin="anonymous"
+></script>
+
+<!-- Bootstrap -->
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+    crossorigin="anonymous"
+/>
+
+<!-- FontAwesome -->
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+    crossorigin="anonymous"
+/>
+```
