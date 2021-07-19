3336ef15cf448f4d16832af0bb48f8b3afe22d40
FinallyPeace <ll7856l@gmail.com>
2021-07-19 17:52:20 +0800

    improve rwd

--- 4. 連動表單/vue.js
+++ 4. 連動表單/vue.js
@@ -4,22 +4,22 @@ let data = {
         content: null
     },
     menu: [
-        { type: '沙拉', content: '煙燻鮭魚', link: '/' },
-        { type: '沙拉', content: '炭烤雞肉', link: '/' },
-        { type: '沙拉', content: '鮮蝦蘆筍', link: '/' },
-        { type: '沙拉', content: '水果生菜', link: '/' },
-        { type: '主餐', content: '菲力牛排', link: '/' },
-        { type: '主餐', content: '德國豬腳', link: '/' },
-        { type: '主餐', content: '法式羊排', link: '/' },
-        { type: '主餐', content: '清蒸龍蝦', link: '/' },
-        { type: '甜點', content: '焦糖布蕾', link: '/' },
-        { type: '甜點', content: '千層蛋糕', link: '/' },
-        { type: '甜點', content: '提拉米蘇', link: '/' },
-        { type: '甜點', content: '抹茶冰淇淋', link: '/' },
-        { type: '飲料', content: '美式咖啡', link: '/' },
-        { type: '飲料', content: '東方美人茶', link: '/' },
-        { type: '飲料', content: '紅茶拿鐵', link: '/' },
-        { type: '飲料', content: 'Mojito', link: '/' }
+        { type: '沙拉', content: '煙燻鮭魚', link: '$200' },
+        { type: '沙拉', content: '炭烤雞肉', link: '$180' },
+        { type: '沙拉', content: '鮮蝦蘆筍', link: '$180' },
+        { type: '沙拉', content: '水果生菜', link: '$120' },
+        { type: '主餐', content: '菲力牛排', link: '$899' },
+        { type: '主餐', content: '德國豬腳', link: '$699' },
+        { type: '主餐', content: '法式羊排', link: '$899' },
+        { type: '主餐', content: '清蒸龍蝦', link: '$999' },
+        { type: '甜點', content: '焦糖布蕾', link: '$80' },
+        { type: '甜點', content: '千層蛋糕', link: '$130' },
+        { type: '甜點', content: '提拉米蘇', link: '$150' },
+        { type: '甜點', content: '抹茶冰淇淋', link: '$120' },
+        { type: '飲料', content: '美式咖啡', link: '$80' },
+        { type: '飲料', content: '東方美人茶', link: '$60' },
+        { type: '飲料', content: '紅茶拿鐵', link: '$70' },
+        { type: '飲料', content: 'Mojito', link: '$120' }
     ]
 }
 let vm = new Vue({
